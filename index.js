b5f276a6e8853b5b7900a112a7a383994d5e21e2
Leonel Nardelli <leonardelli@MacBook-Pro-de-Leonel.local>
2019-11-28 12:42:52 -0300

    typed.js index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 $(function(){
   //TYPED JS
  var options = {
-   strings: ["Puntúa tus batallas", "Conviértete en jurado","Juega con tus amigos","NUEVOS FORMATOS","versión 2.0 beta", "<h3>¿Estás Listo?" ],
+   strings: ["    ","Puntúa tus batallas", "Conviértete en jurado","Juega con tus amigos","NUEVOS FORMATOS","versión 2.0 beta", "<h3>¿Estás Listo?" ],
    typeSpeed: 120,
    showCursor: false,
    smartBackspace: true
